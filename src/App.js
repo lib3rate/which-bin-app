bd11f186c48992ea6678f9a6c0522b2cb4bfec00
Dmitry <dmitry.petrov0918@gmail.com>
2020-08-01 14:57:00 -0400

    Add react router and basic routes to render

--- src/App.js
+++ src/App.js
@@ -1,26 +1,107 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/new">Capture a photo</Link>
+          </li>
+          <li>
+            <Link to="/forest">Forest</Link>
+          </li>
+          <li>
+            <Link to="/users">Your userpage</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/new">
+            <New />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/forest">
+            <Forest />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Register() {
+  return <h2>Register</h2>;
+}
+
+function Login() {
+  return <h2>Login</h2>;
+}
+
+function New() {
+  return <h2>Capture a photo</h2>;
+}
+
+function Forest() {
+  return <h2>This is your Forest</h2>;
+}
+
+function Users() {
+  let match = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Users</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/1`}>User 1</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/2`}>User 2</Link>
+        </li>
+      </ul>
+
+      {/* The Users page has its own <Switch> with more routes
+          that build on the /topics URL path. You can think of the
+          2nd <Route> here as an "index" page for all users, or
+          the page that is shown when no topic is selected */}
+      <Switch>
+        <Route path={`${match.path}/:id`}>
+          <Userpage />
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a user.</h3>
+        </Route>
+      </Switch>
     </div>
   );
 }
 
-export default App;
+function Userpage() {
+  let { id } = useParams();
+  return <h3>Requested user ID: {id}</h3>;
+}

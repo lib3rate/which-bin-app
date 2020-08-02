a7c3fea1081da6839e8a1b0851fc067915c57531
Dmitry <dmitry.petrov0918@gmail.com>
2020-08-02 15:42:24 -0400

    Add styled components library

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import styled from 'styled-components'
 
-export default function Button(props) {
-  return (
-    <>
-      <button
-        onClick={props.onClick}
-      >
-        {props.children}
-      </button>
-    </>
-  )
-}
+const Button = styled.button`
+  background: darkorange;
+  border-radius: 3px;
+  border: 2px solid darkorange;
+  color: white;
+  margin: 0 1rem;
+  padding: 0.25rem 1em;
+`
+
+export { Button as default }
+
+// export default function Button(props) {
+//   return (
+//     <>
+//       <button
+//         onClick={props.onClick}
+//       >
+//         {props.children}
+//       </button>
+//     </>
+//   )
+// }

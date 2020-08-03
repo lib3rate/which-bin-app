af8aab14c69a9e9bdf64b8bb5aca6f86387723e3
Dmitry <dmitry.petrov0918@gmail.com>
2020-08-03 14:32:29 -0400

    Update the button colour

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ import Button from '@material-ui/core/Button';
 const MyButton = styled(Button)({
   // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
   display: "block",
-  background: 'linear-gradient(45deg, #760078 30%, #b100c4 90%)',
+  background: 'linear-gradient(45deg, #ff4000 30%, #ff7b4f 90%)',
   border: 0,
   borderRadius: 3,
   boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
